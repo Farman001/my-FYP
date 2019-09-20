cfb1f8e664cc1ea88f217fc065c576928234116c
Farman001 <32383397+Farman001@users.noreply.github.com>
2019-09-21 00:14:27 +0700

    Update calib.py

--- calib.py
+++ calib.py
@@ -1,14 +1,19 @@
 import numpy as np
 import cv2 as cv
 import glob
-
+import matplotlib.pyplot as plt
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 25, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
+x, y = [], []
+for i in range(6):
+	for j in range(9):
+		x.append(25*j), y.append(25*i)
+nx=np.asarray(x)
+ny=np.asarray(y)
+objp=np.hstack((nx.reshape(54,1),ny.reshape(54,1),np.zeros((54,1)))).astype(np.float32)
+print(objp)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -24,16 +29,16 @@ for fname in images:
     print('reading the image', fname)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6),None)
+    ret, corners = cv.findChessboardCorners(gray, (9,6),None)
 
 
     # If found, add object points, image points (after refining them)
     if ret == True:
        	print('pattern found')
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        img = cv.drawChessboardCorners(img, (7,6), corners2,ret)
-        cv.imshow('img', img)
-        cv.waitKey(500)
+        img = cv.drawChessboardCorners(img, (9,6), corners2,ret)
+        #plt.imshow( img)
+        #plt.show()
         #k=cv.waitKey(0) & 0xFF
         #if k==25:
         #	print('image skipped')
@@ -51,7 +56,7 @@ cv.destroyAllWindows()
 if npatternfound>1:
 	print(npatternfound, " good images found")
 	ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-	img = cv.imread('/home/farman/Documents/my-FYP/IMG_20190224_162005.jpg')
+	img = cv.imread('/home/farman/Downloads/object1.jpg')
 	h,  w = img.shape[:2]
 	newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 	mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
@@ -59,15 +64,21 @@ if npatternfound>1:
 
 	# crop the image
 	x, y, w, h = roi
+	print(roi)
 	dst = dst[y:y+h, x:x+w]
 	cv.imwrite('calibresult.png', dst)
+	print(ret)
 	print('calibration matrix')
 	print(mtx)
 	print('distortion coefficients')
 	print(dist)
-	mean_error = 0
+	total_error = 0
 	for i in range(len(objpoints)):
 	    imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
 	    error = cv.norm(imgpoints[i], imgpoints2, cv.NORM_L2)/len(imgpoints2)
-	    mean_error += error
-	print( "total error: {}".format(mean_error/len(objpoints)) )
+	    total_error += error
+	print( "mean error: {}".format(total_error/len(objpoints)) )
+
+print('rotational verstors:', rvecs)
+print('???????????????????????????????????????????????')
+print('translational vectors', tvecs)
